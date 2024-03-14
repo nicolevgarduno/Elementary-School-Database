f58ab75b953035dffe578159aa88cde3eb123862
nicolevgarduno <65866620+nicolevgarduno@users.noreply.github.com>
2024-03-14 10:17:03 -0500

    Combine Parents & Staff tables.sql
    Combined Gia's and Nicole's first pull request. It combines basic student info and basic staff info into one page

--- Parents.sql
+++ Parents.sql
@@ -12,6 +12,28 @@ INSERT INTO Parentss(ID,Name,ChildName,PhoneNumber) VALUES
     ('2','Leah Daughtler','Karter Daughtler','206716289'),
     ('3','Eva Brown','Luna Brown','6592726829'),
     ('4','Mila Mitchell','Quinn Mitchell','878286268');
-    
+
+
+
+CREATE TABLE StaffTables (
+	ID INT,
+	StaffFirstName VARCHAR(100),
+    StaffLastName VARCHAR(100),
+    StaffPosition VARCHAR(100),
+    EducationLevel VARCHAR(100),
+    TeachingAges VARCHAR(100)
+);
+
+INSERT INTO StaffTables (ID, StaffFirstName, StaffLastName, StaffPosition, EducationLevel, TeachingAges) VALUES
+(1,'Kimberly', 'Baker', 'Teacher', 'B', 'First'),
+(2, 'Brooks', 'Taylor', 'Aide', 'HS', 'First'),
+(3, 'Charlie', 'Moore', 'Teacher', 'B', 'Second'),
+(4, 'Samuel', 'Bandini', 'Aide', 'HS', 'Second'),
+(5, 'Brooks', 'Brown', 'Teacher', 'B', 'Third'),
+(6, 'Evertett', 'Rivera', 'Aide', 'HS', 'Third'),
+(7, 'Clara', 'Jones', 'Teacher', 'B', 'Fourth'),
+(8, 'Juniper', 'Gonzalez', 'Aide', 'HS', 'Fourth'),
+(9, 'Robert', 'Ramirez', 'Teacher', 'B', 'Fifth'),
+(10, 'Addison', 'Lewis', 'Aide', 'HS', 'Fifth');
 
 
