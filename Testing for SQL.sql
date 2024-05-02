1ae4f1c2599cc72eae09a59a3f8a0b670384db9b
A1jandro-Jimenez <160904165+A1jandro-Jimenez@users.noreply.github.com>
2024-05-02 10:14:37 -0500

    Add files via upload

---
+++ Testing for SQL.sql
@@ -0,0 +1,14 @@
+INSERT INTO Staff (ID, StaffFirstName, StaffLastName, StaffPosition, EducationLevel, TeachingAges)
+VALUES
+(13, , , , ,), 
+(14, , , , ,), 
+(15, , , , ,), 
+(16, , , , ,), 
+(17, , , , ,), 
+(18, , , , ,), 
+(19, , , , ,), 
+(20, , , , ,), 
+(21, , , , ,), 
+(22, , , , ,), 
+(23, , , , ,), 
+(24, , , , ,), 
